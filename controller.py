d01991b973debb0a29dbdfd52d478da2372335ab
Griffin Ansel <griffin.ansel@gmail.com>
2021-06-09 16:49:12 -0400

    Adding basic functionality in execute, such as pressing the shift key.

--- controller.py
+++ controller.py
@@ -11,5 +11,12 @@ parser.add_argument('--runtime', required=False, type=int, help='Please specify 
 run_time = parser.parse_args().runtime
 
 def run():
+    """
+    Method to run AppearOnline
+    """
     AO = AppearOnline()
-    AO.execute(run_time)
+    AO.execute(run_time) if (run_time is not None) else AO.execute()
+
+
+if __name__=="__main__":
+    run()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Main
 import sys
 import time
 
-import pyautogui
+import pyautogui as pya
 
 class AppearOnline:
     """
@@ -14,6 +14,17 @@ class AppearOnline:
     def __init__(self):
         pass
 
-    def execute(self, run_time):
-        pass
+    def minute_passed(self) -> bool:
+        time.sleep(60)
+        return True
+
+    def execute(self, run_time: int = 300):
+        runtime_counter = 0
+        while run_time > runtime_counter:
+            pya.press("shift")
+            minute = self.minute_passed()
+            if minute:
+                runtime_counter += 1
+
+
 
