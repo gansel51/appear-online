7cb2dc5a570e744d617ab679db8c2de91fcec8fe
Griffin Ansel <griffin.ansel@gmail.com>
2021-06-09 15:45:13 -0400

    Adding basic structure and requirements file.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Main
+"""
+
+import pyautogui
+
+class AppearOnline:
+    """
+    Appear online class
+    """
+
+    def __init__(self):
+        pass
+
+    def controller(self):
+        pass
