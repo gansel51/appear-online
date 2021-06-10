b82ad115151061f2d90f5214d1acbc71475e4d6a
Griffin Ansel <griffin.ansel@gmail.com>
2021-06-10 00:46:00 -0400

    Utilizing the mouse library instead of pyautogui .

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """
 Main
 """
-import sys
 import time
 
-import pyautogui as pya
+import mouse
 
 class AppearOnline:
     """
@@ -21,7 +20,8 @@ class AppearOnline:
     def execute(self, run_time: int = 300):
         runtime_counter = 0
         while run_time > runtime_counter:
-            pya.press("shift")
+            mouse.press(button='left')
+            mouse.release(button='left')
             minute = self.minute_passed()
             if minute:
                 runtime_counter += 1
